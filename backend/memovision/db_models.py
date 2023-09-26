8f071a29d9a9d8e41f3c57f280443b0c24fbd27a
Štěpán Miklánek <smiklanek@gmail.com>
2023-09-26 17:06:40 +0200

    merge main with dev (#1)
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * change db name
    
    * change db name
    
    ---------
    
    Co-authored-by: Matej Istvanek <60104814+xistva02@users.noreply.github.com>

--- backend/memovision/db_models.py
+++ backend/memovision/db_models.py
@@ -58,7 +58,6 @@ class Track(db.Model):
     num_bad_regions = db.Column(db.Integer, unique=False)
     gt_measures = db.Column(db.Boolean, unique=False, default=False)
     tf_measures = db.Column(db.Boolean, unique=False, default=False)
-    # one-to-many relationship with regions and labels
     regions = db.relationship('TrackRegion', backref='track', passive_deletes=True)
     diff_regions = db.relationship('DiffRegion', backref='track', passive_deletes=True)
     labels = db.relationship('TrackLabel', backref='track', passive_deletes=True, order_by='TrackLabel.label_name')
