626d4b24c8a859b3dc141c8407fee768c9e54ccd
Štěpán Miklánek <smiklanek@gmail.com>
2023-11-27 18:42:21 +0100

    chords fetch fix

--- backend/memovision/_modules/track_manager/sync_routes.py
+++ backend/memovision/_modules/track_manager/sync_routes.py
@@ -141,13 +141,6 @@ colors_dict = {
     'A#': '#9A6324'
 }
 
-colors = [
-    '#e6194B', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4',
-    '#42d4f4', '#f032e6', '#bfef45', '#fabed4', '#469990', '#dcbeff',
-    '#9A6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1',
-    '#000075', '#a9a9a9', '#ffffff', '#000000'
-]
-
 
 def get_chords(path):
     chords_list = []
@@ -181,7 +174,9 @@ def chords():
     if ref_track:
         ref_chords = f'./user_uploads/{current_user.username}/{current_user.selected_session}/{ref_track.filename}/features/chords.txt'
         chords_list = get_chords(ref_chords)
-    return jsonify({'message': 'success', 'chordsList': chords_list})
+        return jsonify({'message': 'success', 'chordsList': chords_list})
+    else:
+        return jsonify({'message': 'chords do not exist'})
 
 
 @sync_routes.route('/find-duplicates', methods=['GET'])
